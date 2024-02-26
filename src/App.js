2069fcf74639359a41f4f36b26164cce172cec06
Александр <sivko8898@gmai.com>
2024-02-26 15:10:31 +0200

    Add banner button component

--- src/App.js
+++ src/App.js
@@ -4,30 +4,19 @@ import ButtonBlackBasket from "./components/Button/ButtonBlackBasket/ButtonBlack
 import ButtonOrangArrow from "./components/Button/ButtonOrang/ButtonOrang";
 import ButtonResponseWhite from "./components/Button/ButtonResponse/White/ButtonResponseWhite";
 import SliderCircle from "./components/Button/SliderCircle/SliderCircle";
-import './App.css';
-import './reset.scss'
-import Card from './components/Card/Card'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Banner from "./components/Banner/Banner";
-
-
+import "./App.css";
+import "./reset.scss";
+import Card from "./components/Card/Card";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Banner from "../src/components/Banner/Banner";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Card />  
-      <Header />
-      <ButtonOrangArrow text="GO TO DIRECTORY"
-      />
-      <ButtonOrangArrow text="MORE DETAIL"/>
-      <ButtonBlackArrow text="MORE DETAIL"/>
-      <ButtonBlackBasket />
-      <ButtonResponseWhite />
-      <SliderCircle />
-      
-     <Banner />
+      <Banner />
+      <Card />
       <Footer />
     </div>
   );
