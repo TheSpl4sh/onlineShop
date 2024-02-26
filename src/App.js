43fefee3a4789904b6cd42adb6487278d80eb49e
Александр <sivko8898@gmai.com>
2024-02-27 00:05:29 +0200

    Resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import "./App.css";
-import ButtonBlackArrow from "./components/Button/ButtonBlackArrow/ButtonBlackArrow";
-import ButtonBlackBasket from "./components/Button/ButtonBlackBasket/ButtonBlackBasket";
-import ButtonOrangArrow from "./components/Button/ButtonOrang/ButtonOrang";
-import ButtonResponseWhite from "./components/Button/ButtonResponse/White/ButtonResponseWhite";
-import SliderCircle from "./components/Button/SliderCircle/SliderCircle";
+// import ButtonBlackArrow from "./components/Button/ButtonBlackArrow/ButtonBlackArrow";
+// import ButtonBlackBasket from "./components/Button/ButtonBlackBasket/ButtonBlackBasket";
+// import ButtonOrangArrow from "./components/Button/ButtonOrang/ButtonOrang";
+// import ButtonResponseWhite from "./components/Button/ButtonResponse/White/ButtonResponseWhite";
+// import SliderCircle from "./components/Button/SliderCircle/SliderCircle";
 import "./App.css";
 import "./reset.scss";
-import Card from "./components/Card/Card";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+// import Card from "./components/Card/Card";
+// import Header from "./components/Header/Header";
+// import Footer from "./components/Footer/Footer";
+import PlacingOrder from "./Pages/PlacingOrder/PlacingOrder"
 import Banner from "../src/components/Banner/Banner";
 
 function App() {
   return (
     <div className="App">
-      
-      <Header />
-      <ButtonBlackArrow text="MORE DETAIL" />
+      < PlacingOrder />
+      {/* <ButtonBlackArrow text="MORE DETAIL" />
       <ButtonBlackBasket text="ADD TO BASKET" />
       <ButtonOrangArrow text="MORE DETAIL" />
+      
+      <Card /> */}
       <Banner />
-      <Card />
-      <Footer />
     </div>
   );
 }
