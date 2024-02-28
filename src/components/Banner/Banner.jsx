f1725dfc3a0214108bd10994b311ad6b7ab9bfdf
Александр <sivko8898@gmai.com>
2024-02-28 22:06:41 +0200

    oleksandr-banner-button

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -34,6 +34,7 @@ function Banner() {
               technology opens through perforated insole.
             </li>
             <img className="sneakerBanners" src={Group} alt="Group" />
+
             <li className="BannerPrice">
               <span className="banner-from">from</span>
               <span className="banner-price">
@@ -44,14 +45,18 @@ function Banner() {
           <div className="buttonBanner">
             <ButtonBlackArrow text="MORE DETAIL" />
           </div>
-
-          <div className="sliderCircle">
-            <SliderCircle />
-          </div>
         </div>
         <img className="sneakerBanner" src={Group} alt="Group" />
-          <SliderTimer className="banner-sliderTimer"/>
-          
+        <div className="banner-sliderTimers">
+          <SliderTimer />
+        </div>
+      </div>
+      <div className="sliderCircle">
+        <SliderCircle />
+      </div>
+
+      <div className="banner-sliderTimer">
+        <SliderTimer />
       </div>
       
       </div>
