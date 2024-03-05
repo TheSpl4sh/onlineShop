52b1359535f1220963e27855b00c4be30e1064cb
Oleksii Bernac'kyi <bernatskyioleksii@gmail.com>
2024-03-05 20:15:39 +0200

    fixed header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./reset.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import MainPage from "./Pages/MainPage/MainPage";
+import AllProductsPage from "./Pages/AllProductsPage/AllProductsPage"
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AllProductsPage />
       <MainPage />
       <Footer />
     </div>
