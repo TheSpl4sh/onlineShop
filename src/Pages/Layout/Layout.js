cdae8beaf676f1ec06f3193df55f8475aff578b8
TheSpl4sh <thesplash007@gmail.com>
2024-03-06 14:00:14 +0200

    fix: App.js, Layout.js

--- src/Pages/Layout/Layout.js
+++ src/Pages/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-import { Outlet } from 'react-router'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
