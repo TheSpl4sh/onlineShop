fd9102976ff501f84b8c877e1a9107a4f684ec71
Olya Tyshchenko <tyshchenkoola@gmail.com>
2024-03-14 20:28:10 +0200

    added page "Contacts" and component "ContactsForm"

---
+++ src/App 2.js
@@ -0,0 +1,24 @@
+import './App.css';
+import './reset.scss'
+// import Card from './components/Card/Card';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import MainPage from './Pages/MainPage/MainPage'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header />
+      <MainPage />
+      <Footer />
+
+
+    </div>
+  );
+}
+
+export default App;
