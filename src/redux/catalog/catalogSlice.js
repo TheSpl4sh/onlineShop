ea331b41c6e2f3eb29028e72aeb8d89a63e5f1de
TheSpl4sh <thesplash007@gmail.com>
2024-03-17 14:52:24 +0200

    feat: productSlice, re-write objects

---
+++ src/redux/catalog/catalogSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const loadProducts = createAsyncThunk(
+    'products/loadAll',
+
+    async () => {
+        const res = await fetch('http://localhost:4000/api/catalog');
+        
+        if(!res.ok) {
+          throw new Error(`Can't fetch`)
+        }
+        
+        const data = await res.json();
+        return data
+      }
+)
+
+const catalogSlice = createSlice({
+    name: 'products',
+    initialState: {
+        products: []
+      },
+      extraReducers: (builder) => {
+        builder
+          .addCase(loadProducts.fulfilled, (state,action) => {
+            state.products = action.payload 
+          })
+        }
+})
+
+export default catalogSlice.reducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/authSlice';
+import catalogReducer from './catalog/catalogSlice';
 
 const store = configureStore({
     reducer: {
         loginCustomer: authReducer,
+        products: catalogReducer
     },
   });
 
