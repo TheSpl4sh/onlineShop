d7be9e630a943b49259496622078d86a936740a2
Oleksii Bernac'kyi <bernatskyioleksii@gmail.com>
2024-03-24 18:41:40 +0200

    git commit -m "modified: /Card.js"

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,57 +1,57 @@
-import React from "react";
-// import Slider from "../Slider/Slider";
-import './vertical-card.scss';
-// import sneakerSmall from './images/sneaker-small.png';
-import { Link } from "react-router-dom";
+// import React from "react";
+// // import Slider from "../Slider/Slider";
+// import './vertical-card.scss';
+// // import sneakerSmall from './images/sneaker-small.png';
+// import { Link } from "react-router-dom";
 
 
-// const Card = ({ item }) => {
+// // const Card = ({ item }) => {
     
-    return (
-            <div className="vertical-card">
-                <div className="vertical-card__img-container">
-                    <Link to='card'>
-                        <img src={item.logo_img} alt="sneaker" draggable="false" className="vertical-card__logo" />
-                    </Link>
+//     return (
+//             <div className="vertical-card">
+//                 <div className="vertical-card__img-container">
+//                     <Link to='card'>
+//                         <img src={item.logo_img} alt="sneaker" draggable="false" className="vertical-card__logo" />
+//                     </Link>
 
-//                     <div className="vertical-card__chips vertical-card__chips--new">{item.chips}</div>
-//                     <button className="favorite-button"></button>
+// //                     <div className="vertical-card__chips vertical-card__chips--new">{item.chips}</div>
+// //                     <button className="favorite-button"></button>
 
-//                     {/* <Slider /> */}
-//                 </div>
+// //                     {/* <Slider /> */}
+// //                 </div>
 
 
-                <div className="vertical-card__description">
-                        <span className="vertical-card__gender">
-                            {item.parentId === "men-clothing"
-                                ? "ЖІНОЧІ"
-                                : "ЧОЛОВІЧІ" }
-                        </span>
+//                 <div className="vertical-card__description">
+//                         <span className="vertical-card__gender">
+//                             {item.parentId === "men-clothing"
+//                                 ? "ЖІНОЧІ"
+//                                 : "ЧОЛОВІЧІ" }
+//                         </span>
 
-//                         <h4  className="vertical-card__name">
-//                             {item.name}
-//                         </h4>
+// //                         <h4  className="vertical-card__name">
+// //                             {item.name}
+// //                         </h4>
 
-//                         <div className="vertical-card__colors-block">
-//                             <span className="vertical-card__colors-span">Кольори:</span>
+// //                         <div className="vertical-card__colors-block">
+// //                             <span className="vertical-card__colors-span">Кольори:</span>
                             
-//                             <button  className="vertical-card__sneaker-color vertical-card__sneaker-color--grey"></button>
-//                             <button  className="vertical-card__sneaker-color vertical-card__sneaker-color--red"></button>
-//                             <button  className="vertical-card__sneaker-color vertical-card__sneaker-color--black"></button>
-//                         </div>
+// //                             <button  className="vertical-card__sneaker-color vertical-card__sneaker-color--grey"></button>
+// //                             <button  className="vertical-card__sneaker-color vertical-card__sneaker-color--red"></button>
+// //                             <button  className="vertical-card__sneaker-color vertical-card__sneaker-color--black"></button>
+// //                         </div>
                         
-//                         <div className="vertical-card__price-block">
-//                             <span className="vertical-card__price-number vertical-card__price-number--actual">
-//                                 {item.price}
-//                             </span>
+// //                         <div className="vertical-card__price-block">
+// //                             <span className="vertical-card__price-number vertical-card__price-number--actual">
+// //                                 {item.price}
+// //                             </span>
 
-//                             {/* <span className="vertical-card__price-number vertical-card__price-number--discount">
-//                                 11 699 ₴
-//                             </span> */}
-//                             <button className="cart-button"></button>
-//                         </div>
-//                 </div>
-//             </div>
+// //                             {/* <span className="vertical-card__price-number vertical-card__price-number--discount">
+// //                                 11 699 ₴
+// //                             </span> */}
+// //                             <button className="cart-button"></button>
+// //                         </div>
+// //                 </div>
+// //             </div>
 //     )
 // }
 
