6b7df36cbbeb23afa092fa45422c4850a921ed4b
Oleksii Bernac'kyi <bernatskyioleksii@gmail.com>
2024-03-27 20:50:26 +0200

    git commit -m "modified: /AllProductsPage.js,/CustomizedSlider.js,/ModalMenu.js,"

--- src/Pages/AllProductsPage/components/CustomizedSlider/CustomizedSlider.js
+++ src/Pages/AllProductsPage/components/CustomizedSlider/CustomizedSlider.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from "prop-types";
 import Slider, { SliderThumb } from "@mui/material/Slider";
 import { styled } from "@mui/material/styles";
